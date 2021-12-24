81d69edf82220603cc4231cdd4c56e783a3d896c
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-23 21:28:19 -0500

    Create rounds for playing rock paper scissors, now I have to make it multiple rounds

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,19 +7,38 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         return "It's a tie!";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){
-        return "Player wins this round!";
+        return "Player Wins!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock"){
-        return "Player wins this round!";
+        return "Player Wins!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
-        return "Player wins this round!";
+        return "Player Wins!";
     } else {
         return "Computer Wins!"; 
     }
 }
 
-const playerSelection = "Rock";
-const computerSelection = computerPlay();
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+let computerWin = 1;
+let playerWin = 1;
+function fullRound(){
+    const computerSelection = computerPlay();
+    let playerSelection = prompt("rock, paper, scissors?");
+
+    function roundResult(){
+        let result = playRound(playerSelection, computerSelection);
+            if(result === "Player Wins!"){
+                playerWin ++;
+            } else if (result === "Computer Wins!"){
+                computerWin ++;}
+        
+        return `You played ${playerSelection}
+        Computer played ${computerSelection}
+        ${result} 
+        The score is player ${playerWin - 1} to computer ${computerWin - 1}`;
+    }
+        
+    return roundResult();
+}
+
+console.log(fullRound())
