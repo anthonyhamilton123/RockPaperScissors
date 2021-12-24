0e9867982c5b54651f677f98380fe1200122a7b7
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-23 20:33:02 -0500

    Make playRound function, with console logs to check to make sure the code is working

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,4 +3,23 @@ function computerPlay(){
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection){
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "It's a tie!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){
+        return "Player wins this round!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock"){
+        return "Player wins this round!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
+        return "Player wins this round!";
+    } else {
+        return "Computer Wins!"; 
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
