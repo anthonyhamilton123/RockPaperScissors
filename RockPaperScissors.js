0f0a7e089e29e022788fdcbba1ae84e739af18b4
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-23 21:39:43 -0500

    Added comments (working on cleaner code)

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,8 +1,12 @@
+//Rock Paper Scissors Console game in JS as part of the Odin Project!
+
+//Computer move pulled from a random number generator
 function computerPlay(){
     arr = ['Rock','Paper','Scissors']
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
+//Define playing a round, tie, player win, computer win
 function playRound(playerSelection, computerSelection){
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         return "It's a tie!";
@@ -18,13 +22,19 @@ function playRound(playerSelection, computerSelection){
 }
 
 
-
+//these variables are the integer of wins, 1 and then minused for ease
 let computerWin = 1;
 let playerWin = 1;
+
+//this function defines the full scope of the round
 function fullRound(){
     const computerSelection = computerPlay();
+
+    //prompt for the player to select rock, paper, scissors
+    //could add a way for invalid input
     let playerSelection = prompt("rock, paper, scissors?");
 
+    //adds to the respective win column
     function roundResult(){
         let result = playRound(playerSelection, computerSelection);
             if(result === "Player Wins!"){
@@ -41,6 +51,7 @@ function fullRound(){
     console.log(roundResult());
 }
 
+//game to five, will update to while loop
 function game(){
     fullRound();
     fullRound();
